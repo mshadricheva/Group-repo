fe27a33b7db897138f61bd62dd2488744996f9d2
mshadricheva <shadricheva@mail.ru>
2020-12-11 11:33:35 +0300

    +

---
+++ build/precache-manifest.a29138b1e17284cb24fd8eeefab2f968.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d532531b1514ecc0e98873b0cadfcb06",
+    "url": "/Group-repo/index.html"
+  },
+  {
+    "revision": "0e68f88111f999af9b71",
+    "url": "/Group-repo/static/css/main.4641f65a.chunk.css"
+  },
+  {
+    "revision": "23a9f84cb11d03a13a85",
+    "url": "/Group-repo/static/js/2.f904b403.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Group-repo/static/js/2.f904b403.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e68f88111f999af9b71",
+    "url": "/Group-repo/static/js/main.670989e6.chunk.js"
+  },
+  {
+    "revision": "5fbd193ec4c4a5ee2cc1",
+    "url": "/Group-repo/static/js/runtime-main.78dd62e8.js"
+  }
+]);
